58ddd11d4c0ad6b255827d443a961796e2c77430
giridhersadineni <giridher.sadineni@gmail.com>
2019-12-26 19:14:34 +0530

    Done with Slider v2

---
+++ 7.Map & Filter/arraymanipulation.js
@@ -0,0 +1,17 @@
+let numbers=[1,2,3,4,5,6];
+
+
+
+numbers.forEach(function(n){
+     console.log(n*2);
+})
+
+
+
+for(i=0;i<numbers.length;i++){
+    printtoconsole(numbers[i]);
+}
+
+function printtoconsole(n){
+    console.log(n*2);
+}
